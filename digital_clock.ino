9bd632a8b6f9277c2d3bc633b18d34b09d2a1072
mocl125 <spicytaco361@gmail.com>
2015-10-05 09:54:48 -0500

    Included "SevSeg.h" library into code
    Included SevSeg.h library into code to display number from library. No
    success yet.

--- digital_clock.ino
+++ digital_clock.ino
@@ -2,6 +2,7 @@
 // 
 
 #include "SevSeg.h"
+        
 
 #define d1 13
 #define a  2
@@ -15,56 +16,19 @@
 #define dp 9
 #define d  5
 #define e  6
+
+SevSeg sevseg;
+
 void setup() {
   // put your setup code here, to run once:
-  pinMode(d1, OUTPUT);
-  pinMode(a,OUTPUT);
-  pinMode(f, OUTPUT);
-  pinMode(d2, OUTPUT);
-  pinMode(d3, OUTPUT);
-  pinMode(b, OUTPUT);
-  pinMode(d4, OUTPUT);
-  pinMode(g, OUTPUT);
-  pinMode(c, OUTPUT);
-  pinMode(dp, OUTPUT);
-  pinMode(d, OUTPUT);
-  pinMode(e, OUTPUT);
-
+byte numDigits = 4;
+   byte digitPins[] = {d1, d2, d3, d4};
+   byte segmentPins[] = {a, b, c, d, e, f, g, dp};
+   sevseg.begin(COMMON_ANODE, numDigits, digitPins, segmentPins);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  //  digitalWrite(a, LOW);
-//  digitalWrite(b, LOW);
-//  digitalWrite(c, LOW);
-//  digitalWrite(d, LOW);
-//  digitalWrite(e, LOW);
-//  digitalWrite(f, LOW);
-//  digitalWrite(g, LOW);
-  
-  digitalWrite(d1, LOW);
-  digitalWrite(d2, LOW);
-  digitalWrite(d3, LOW);
-  digitalWrite(d4, LOW);
-  digitalWrite(dp,LOW);
-  
-  
-  digitalWrite(f, HIGH);
-  digitalWrite(b, HIGH);
-  digitalWrite(g, HIGH);
-  digitalWrite(c, HIGH);
-  digitalWrite(d, HIGH);
-  digitalWrite(e, HIGH);
-  digitalWrite(a, HIGH);
-
-  delay(1000);
-
-  digitalWrite(d1, HIGH);
-  digitalWrite(d2,HIGH);
-  digitalWrite(d3, HIGH);
-  digitalWrite(d4, HIGH);
-  digitalWrite(dp,HIGH);
-  delay(1000);
-
-  
+sevseg.setNumber(1,1);
+  sevseg.refreshDisplay();
 }
